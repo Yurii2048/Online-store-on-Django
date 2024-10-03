c709a965a1edb38e99442b29db3a89ecbbb969e9
LESIUK <iuriilesyuk@gmail.com>
2024-10-03 13:08:11 +0300

    Slug converter for product

--- goods/views.py
+++ goods/views.py
@@ -10,41 +10,17 @@ def catalog(request):
     context = {
         "title": "Home - Каталог",
         "goods": goods,
-        # [
-        #     {
-        #         "image": "deps/images/goods/sofa.jpg",
-        #         "name": "Диван обыкновенный",
-        #         "description": "Диван, он же софа обыкновенная, ничего примечательного для описания.",
-        #         "price": 190.00,
-        #     },
-        #     {
-        #         "image": "deps/images/goods/office chair.jpg",
-        #         "name": "Стул офисный",
-        #         "description": "Описание товара, про то какой он классный, но это стул, что тут сказать...",
-        #         "price": 30.00,
-        #     },
-        #     {
-        #         "image": "deps/images/goods/plants.jpg",
-        #         "name": "Растение",
-        #         "description": "Растение для украшения вашего интерьера подарит свежесть и безмятежность обстановке.",
-        #         "price": 10.00,
-        #     },
-        #     {
-        #         "image": "deps/images/goods/flower.jpg",
-        #         "name": "Цветок стилизированный",
-        #         "description": "Дизайнерский цветок (возможно искусственный) для украшения интерьера.",
-        #         "price": 15.00,
-        #     },
-        #     {
-        #         "image": "deps/images/goods/strange table.jpg",
-        #         "name": "Прикроватный столик",
-        #         "description": "Столик, довольно странный на вид, но подходит для размещения рядом с кроватью.",
-        #         "price": 25.00,
-        #     },
-        # ],
     }
+
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+
+    product = Products.objects.get(slug=product_slug)
+
+    context = {
+        "product": product,
+    }
+
+    return render(request, "goods/product.html", context)
