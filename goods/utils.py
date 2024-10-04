ad72913747ddc1e303787e4abf48fb40f6a95a82
LESIUK <iuriilesyuk@gmail.com>
2024-10-04 20:56:16 +0300

    PostgreSQL data base connected & full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,7 +1,29 @@
 import keyword
 from django.db.models import Q
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank,
+    SearchVector,
+)
 
 from goods.models import Products
+
+
+# def q_search(query):
+
+#     if query.isdigit() and len(query) <= 5:
+#         return Products.objects.filter(id=int(query))
+
+#     keywords = [word for word in query.split() if len(word) > 2]
+
+#     q_object = Q()
+
+#     for token in keywords:
+#         q_object |= Q(description__icontains=token)
+#         q_object |= Q(name__icontains=token)
+
+#     return Products.objects.filter(q_object)
 
 
 def q_search(query):
@@ -9,12 +31,14 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
 
-    q_object = Q()
+    return Products.objects.annotate(
+        rank=SearchRank(vector, query),
+    ).order_by("-rank")
 
-    for token in keywords:
-        q_object |= Q(description__icontains=token)
-        q_object |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_object)
+    # return Products.objects.filter(description__search=query)
+    # return Products.objects.annotate(
+    #     search=SearchVector("name", "description"),
+    # ).filter(search=query)
--- shop/settings.py
+++ shop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.postgres",
     "debug_toolbar",
     "main",
     "goods",
@@ -79,10 +80,22 @@ WSGI_APPLICATION = "shop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "first_jango_project_db",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
