f929060c020a4893c471cd180049764aaff9d545
LESIUK <iuriilesyuk@gmail.com>
2024-10-07 15:44:53 +0300

    Templates for users app added

--- shop/settings.py
+++ shop/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "main",
     "goods",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,9 @@ ROOT_URLCONF = "shop.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
