ca743f094f3fc291aef2962d853173939f3f7bbe
LESIUK <iuriilesyuk@gmail.com>
2024-10-10 22:05:29 +0300

    Corrective edits

--- orders/views.py
+++ orders/views.py
@@ -82,7 +82,7 @@ from orders.models import Order, OrderItem
 #         return context
 
 
-# @login_required
+@login_required
 def create_order(request):
     if request.method == "POST":
         form = CreateOrderForm(data=request.POST)
